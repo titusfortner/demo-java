26b6d7386d29a5cdb8eea4de3e75b1d6641bfef7
nadvolod <nadvolod@gmail.com>
2020-10-28 14:37:50 -0400

    create a 2nd setup()

--- java8/appium.junit4.framework/src/test/java/com/saucedemo/SimulatorTests.java
+++ java8/appium.junit4.framework/src/test/java/com/saucedemo/SimulatorTests.java
@@ -1,11 +1,38 @@
 package com.saucedemo;
 
+import io.appium.java_client.ios.IOSDriver;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import static org.junit.Assert.assertTrue;
 
 public class SimulatorTests extends TestBase {
+
+    @Before
+    public void setUpSimulatorCaps() throws MalformedURLException {
+        capabilities.setCapability("platformName", "iOS");
+        //cannot use .* for platform version
+        //TODO different from real devices
+        capabilities.setCapability("platformVersion", "13.0");
+        //TODO different from real devices
+        capabilities.setCapability("deviceName", "iPhone XS Max Simulator");
+        capabilities.setCapability("appWaitActivity",
+                "com.swaglabsmobileapp.MainActivity");
+        capabilities.setCapability("name", name.getMethodName());
+        capabilities.setCapability("username", System.getenv("SAUCE_USERNAME"));
+        capabilities.setCapability("accessKey", System.getenv("SAUCE_ACCESS_KEY"));
+        //TODO different from real devices
+        capabilities.setCapability("app",
+                "storage:filename=iOS.Simulator.SauceLabs.Mobile.Sample.app.2.7.0.zip");
+
+        driver = new IOSDriver(new URL(SauceConstants.HubUrl.WEST_FULL),
+                capabilities);
+    }
+
     @Test
     public void shouldOpenApp() {
         assertTrue(getDriver().findElement(By.id("test-LOGIN")).isDisplayed());
--- java8/appium.junit4.framework/src/test/java/com/saucedemo/TestBase.java
+++ java8/appium.junit4.framework/src/test/java/com/saucedemo/TestBase.java
@@ -2,7 +2,6 @@ package com.saucedemo;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
-import io.appium.java_client.ios.IOSDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -10,7 +9,6 @@ import org.junit.rules.TestName;
 import org.openqa.selenium.MutableCapabilities;
 
 import java.net.MalformedURLException;
-import java.net.URL;
 
 public class TestBase {
     @Rule
@@ -19,7 +17,8 @@ public class TestBase {
             return String.format("%s", super.getMethodName());
         }
     };
-    private AppiumDriver<MobileElement> driver;
+    public AppiumDriver<MobileElement> driver;
+    public MutableCapabilities capabilities;
 
     public AppiumDriver<MobileElement> getDriver() {
         return driver;
@@ -35,27 +34,11 @@ public class TestBase {
     // https://wiki.saucelabs.com/display/DOCS/Platform+Configurator#/
     //
     @Before
-    public void setUp() throws MalformedURLException {
-        MutableCapabilities capabilities = new MutableCapabilities();
+    public void setUpSharedCaps() throws MalformedURLException {
+        capabilities = new MutableCapabilities();
         capabilities.setCapability("idleTimeout", "90");
         capabilities.setCapability("noReset", "true");
         capabilities.setCapability("newCommandTimeout", "90");
         capabilities.setCapability("language", "en");
-        capabilities.setCapability("platformName", "iOS");
-        //cannot use .* for platform version
-        //TODO different from real devices
-        capabilities.setCapability("platformVersion", "13.4");
-        //TODO different from real devices
-        capabilities.setCapability("deviceName", "iPhone XS Max Simulator");
-        capabilities.setCapability("appWaitActivity", "com.swaglabsmobileapp.MainActivity");
-        capabilities.setCapability("name", name.getMethodName());
-        capabilities.setCapability("username", System.getenv("SAUCE_USERNAME"));
-        capabilities.setCapability("accessKey", System.getenv("SAUCE_ACCESS_KEY"));
-        //TODO different from real devices
-        capabilities.setCapability("app",
-                "storage:filename=iOS.Simulator.SauceLabs.Mobile.Sample.app.2.7.0.zip");
-
-        driver = new IOSDriver(new URL(SauceConstants.HubUrl.WEST_FULL),
-                capabilities);
     }
 }
